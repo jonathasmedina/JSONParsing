727e9c320e6940f7dceb72e4803e812673ce6620
Jonathas Leontino Medina <emaildojonathas@gmail.com>
2023-11-17 14:52:48 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jsonparsing/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.jsonparsing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Handler mainHandler = new Handler();
+
+    ProgressDialog progressDialog; //deprecated a partir da 26
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new FetchData().start();
+    }
+
+    class FetchData extends Thread{
+        String dados = "";
+
+        @Override
+        public void run() {
+
+            mainHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    progressDialog = new ProgressDialog(MainActivity.this);
+                    progressDialog.setMessage("recuperando dados");
+                    progressDialog.setCancelable(false);
+                    progressDialog.show();
+                }
+            });
+            try {
+                URL url = null;
+                url = new URL("https://jsonplaceholder.org/users");
+
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String linha;
+
+                while((linha  = bufferedReader.readLine()) != null){
+                    dados = dados + linha;
+                }
+                if(!dados.isEmpty()){
+                    Log.e("parse aqui. adicionar para arraylist", dados);
+                }
+
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            mainHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if(progressDialog.isShowing()){
+                        progressDialog.dismiss();
+                    }
+                }
+            });
+        }
+    }
+
+
+}
